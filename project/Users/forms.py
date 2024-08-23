16e11d4f5efc7be70f9f6dfe77a2d095e81e7acf
twy <wtan0106@student.monash.edu>
2024-08-23 12:15:55 +0800

    add logic to show error when user changes username to an existing username

--- project/Users/forms.py
+++ project/Users/forms.py
@@ -1,6 +1,7 @@
 # forms.py
 from django import forms
-from .models import User, History
+from .models import User
+from django.core.exceptions import ValidationError
 
 class SignUpForm(forms.ModelForm):
     class Meta:
@@ -18,5 +19,14 @@ class AuthenticationForm(forms.Form):
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'password', 'country', 'age', 'profile_image', 'gender']
+        fields = ['username', 'password', 'country', 'age', 'gender', 'profile_image']
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        user_id = self.instance.id  # Get the id of the current user being updated
+
+        # Check if a user with the same username exists but not the current user
+        if User.objects.filter(username=username).exclude(id=user_id).exists():
+            raise ValidationError("This username is already taken. All update failed")
+        return username
 
