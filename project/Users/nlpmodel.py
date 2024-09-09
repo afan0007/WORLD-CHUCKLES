f57369fcd7cc9c983ddcf025259ff374a36bb617
twy <wtan0106@student.monash.edu>
2024-09-09 14:51:48 +0800

    testing logout on all pages and fixing model calling function to a working one

--- project/Users/nlpmodel.py
+++ project/Users/nlpmodel.py
@@ -1,10 +1,10 @@
 
 import requests
 
-API_URL = "https://ci8b86d3ag2wz0in.us-east-1.aws.endpoints.huggingface.cloud"
+API_URL = "https://a6g4w0nkhxi9aat9.us-east-1.aws.endpoints.huggingface.cloud"
 headers = {
 	"Accept" : "application/json",
-	"Content-Type": "application/json" 
+	"Content-Type": "application/json"
 }
 
 def query(payload):
@@ -16,7 +16,7 @@ def query(payload):
 # 	"parameters": {}
 # })
 
-# The below dummy function is calling deployed model and generate an output joke 
+# The below dummy function is calling deployed model and generate an output joke
 # def dummy(country, keyword="dummy no keyword"):
 # 	output = query({
 # 	"inputs": "<s> [INST] You are a joke generator. I want a joke related to " + country + " with a quality of 5 out of 5 stars and not offensive. [/INST] ",
@@ -26,8 +26,7 @@ def query(payload):
 # 	return str(output)
 
 def dummy(country, keyword="dummy no keyword"):
-    # Simulated output from the query function
-    # output = [{'generated_text': '<s> [INST] You are a joke generator. I want a joke related to India with a quality of 5 out of 5 stars and not offensive. [/INST] Why did the Indian man go to the doctor? Because he was feeling a little bit "spicy"! </s>'}]
+    # Simulated output from  query function
     output = query({
 	"inputs": "<s> [INST] You are a joke generator. I want a joke related to " + country + " with a quality of 5 out of 5 stars and not offensive. [/INST] ",
 	"parameters": {}
@@ -35,14 +34,14 @@ def dummy(country, keyword="dummy no keyword"):
 
     # Extract the generated text
     generated_text = output[0]['generated_text']
-    
+
     # Find the start and end of the joke
     start = generated_text.find('[/INST] ') + len('[/INST] ')
     end = generated_text.find('</s>')
-    
+
     # Extract the joke
     joke = generated_text[start:end].strip()
-    
+
     print(joke+"hihi")
     return joke
 
